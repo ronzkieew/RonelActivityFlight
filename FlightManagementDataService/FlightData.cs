86b408fcd642b5b3f7d1ad16e7099a342fdda2f9
ronzkieew <ronelaranareyes22102005@gmail.com>
2026-03-13 19:52:29 +0800

    Update FlightData.cs

--- FlightManagementDataService/FlightData.cs
+++ FlightManagementDataService/FlightData.cs
@@ -9,18 +9,6 @@ namespace FlightManagementDataService
 {
     public class FlightData
     {
-        private List<flightModel> flights = new List<flightModel>();
-
-        public FlightDataService()
-        {
-            flights.Add(new flightModel { Origin = "Manila", Destination = "Cebu", Airline = "Philippine Airlines" });
-            flights.Add(new flightModel { Origin = "Iloilo", Destination = "Puerto Princesa", Airline = "Cebu Pacific" });
-            flights.Add(new flightModel { Origin = "Davao", Destination = "Boracay", Airline = "AirAsia" });
-        }
-        public flightModel? GetFlight(string origin, string destination)
-        {
-            return flights.FirstOrDefault(f => f.Origin.ToLower() == origin.ToLower() && f.Destination.ToLower() == destination.ToLower());
-        }
-
+        
     }
 }
